d004b64219f01f7855ff617ea47b5c21fb05574f
Daegyu Han <bigstar77han@gmail.com>
2023-11-16 23:06:41 +0900

    test-node

---
+++ server.js
@@ -0,0 +1,249 @@
+// express 라이브러리 사용위함
+const express = require('express')
+const app = express()
+const { MongoClient, ObjectId } = require('mongodb')
+const methodOverride = require('method-override')
+const bcrypt = require('bcrypt')
+require('dotenv').config()
+
+app.use(methodOverride('_method'))
+app.use(express.static(__dirname + '/public')) // public 폴더 내의 파일을 사용할 수 있게 함 css,js,jpg 파일들(static 파일들)
+app.set('view engine', 'ejs') // ejs setting
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))  // 유저가 데이터를 보냈을 때 꺼내쓸 수 있게 하는 코드
+
+//
+
+// passport 라이브러리 세팅
+const session = require('express-session')
+const passport = require('passport')
+const LocalStrategy = require('passport-local')
+const MongoStore = require('connect-mongo')
+
+app.use(passport.initialize())
+app.use(session({
+  secret: '암호화에 쓸 비번',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { maxAge: 60 * 60 * 1000 },
+  store: MongoStore.create({
+    mongoUrl: 'mongodb+srv://sparta:test@cluster0.edvfknb.mongodb.net/?retryWrites=true&w=majority',
+    dbName: 'forum'
+  })
+}))
+app.use(passport.session())
+//
+
+const { S3Client } = require('@aws-sdk/client-s3')
+const multer = require('multer')
+const multerS3 = require('multer-s3')
+const s3 = new S3Client({
+  region: 'ap-northeast-2',
+  credentials: {
+    accessKeyId: process.env.S3_KEY,
+    secretAccessKey: process.env.S3_SECRET
+  }
+})
+
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: 'bigstarhan33',
+    key: function (요청, file, cb) {
+      cb(null, Date.now().toString()) //업로드시 파일명 변경가능
+    }
+  })
+})
+
+let connectDB = require('./database.js')
+
+let db
+connectDB.then((client) => {
+  console.log('DB연결성공')
+  db = client.db('forum')
+
+  // 서버 띄우는 코드
+  app.listen(process.env.PORT, () => {    //서버 띄울 포트 번호
+    console.log('http://localhost:5000 에서 서버 실행 중')
+  })
+}).catch((err) => {
+  console.log(err)
+})
+// mongoDB library 연결 코드
+
+
+
+
+
+app.get('/', (요청, 응답) => {      //간단한 서버 기능, 누가 메인페이지 접속 시 응답함
+  응답.sendFile(__dirname + '/index.html')
+})
+
+app.get('/news', (요청, 응답) => {
+  응답.send('오늘 비 옴')
+})
+
+app.get('/shop', (요청, 응답) => {
+  응답.send('쇼핑페이지임')
+})
+
+app.get('/list', async (요청, 응답) => {
+  let result = await db.collection('post').find().toArray() // DB 에서 어떤 콜렉션에 있는 모든 DB 를 불러오는 코드
+  // 응답.send(result[0].title)
+
+  응답.render('list.ejs', { 글목록: result })
+})
+
+app.get('/time', async (요청, 응답) => {
+  let time = new Date()
+
+  응답.render('time.ejs', { 시간: time })
+})
+
+app.get('/write', (요청, 응답) => {
+  응답.render('write.ejs')
+})
+
+app.post('/add', async (요청, 응답) => {
+
+  upload.single('img1')(요청, 응답, async (err)=>{
+    if (err) return 응답.send('업로드에러')
+    try {
+      if (요청.body.title == '') {
+        응답.send('제목입력안했음')
+      } else {
+        await db.collection('post').insertOne({ title: 요청.body.title, content: 요청.body.content, img : 요청.file.location })
+        응답.redirect('/list')
+      }
+    } catch (e) {
+      console.log(e)
+      응답.status(500).send('서버에러남')
+    }
+  })
+
+  })
+  
+
+  
+
+app.get('/detail/:id', async (요청, 응답) => {
+
+
+
+  try {
+    let result = await db.collection('post').findOne({ _id: new ObjectId(요청.params.id) })
+    // console.log(result)
+    if (result == null) {
+      응답.status(404).send('이상한 url 입력함')
+    }
+    응답.render('detail.ejs', { 글: result })
+  } catch (e) {
+    console.log(e)
+    응답.status(404).send('이상한 url 입력함')
+  }
+
+})
+
+app.get('/edit/:id', async (요청, 응답) => {
+  let result = await db.collection('post').findOne({ _id: new ObjectId(요청.params.id) })
+  console.log(result)
+  응답.render('edit.ejs', { result: result })
+})
+
+app.put('/edit', async (요청, 응답) => {
+
+  let result = await db.collection('post').updateOne({ _id: new ObjectId(요청.body.id) }, { $set: { title: 요청.body.title, content: 요청.body.content } })
+
+  응답.redirect('/list')
+
+})
+
+
+app.delete('/delete', async (요청, 응답) => {
+  await db.collection('post').deleteOne({ _id: new ObjectId(요청.query.docid) })
+  응답.send('삭제완료')
+})
+
+
+
+app.get('/list/:number', async (요청, 응답) => {
+  let result = await db.collection('post').find().skip((요청.params.number - 1) * 5).limit(5).toArray()
+
+  응답.render('list.ejs', { 글목록: result })
+})
+
+
+app.get('/list/next/:id', async (요청, 응답) => {
+  let result = await db.collection('post').find({ _id: { $gt: new ObjectId(요청.params.id) } }).limit(5).toArray()
+
+  응답.render('list.ejs', { 글목록: result })
+})
+
+
+passport.use(new LocalStrategy(async (입력한아이디, 입력한비번, cb) => {
+  let result = await db.collection('user').findOne({ username: 입력한아이디 })
+  if (!result) {
+    return cb(null, false, { message: '아이디 DB에 없음' })
+  }
+
+
+  if (await bcrypt.compare(입력한비번, result.password)) {
+    return cb(null, result)
+  } else {
+    return cb(null, false, { message: '비번불일치' });
+  }
+}))
+
+// passport.authenticate('local')() 가 실행될 때 마다 아래 코드도 같이 실행됨 ( 세선만드는 코드 )
+
+passport.serializeUser((user, done) => {
+  console.log(user)
+  process.nextTick(() => {
+    done(null, { id: user._id, username: user.username })
+  })
+})
+
+passport.deserializeUser(async (user, done) => {
+  let result = await db.collection('user').findOne({ _id: new ObjectId(user.id) })
+  delete result.password
+  process.nextTick(() => {
+    done(null, result)
+  })
+})
+
+app.get('/login', async (요청, 응답) => {
+  console.log(요청.user)
+  응답.render('login.ejs')
+})
+
+app.post('/login', async (요청, 응답, next) => {
+  passport.authenticate('local', (error, user, info) => {
+    if (error) return 응답.status(500).json(error)
+    if (!user) return 응답.status(401).json(info.message)
+    요청.logIn(user, (err) => {
+      if (err) return next(err)
+      응답.redirect('/list')
+    })
+
+
+  })(요청, 응답, next)
+
+})
+
+app.get('/register', async (요청, 응답) => {
+  응답.render('register.ejs')
+})
+
+app.post('/register', async (요청, 응답) => {
+
+  let 해시 = await bcrypt.hash(요청.body.password, 10)
+
+  await db.collection('user').insertOne({
+    username: 요청.body.username,
+    password: 해시
+  })
+  응답.redirect('/')
+})
+
+app.use('/shop', require('./routes/shop.js'))
+
